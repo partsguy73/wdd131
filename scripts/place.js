9168d0bbb37d26f98340b0110f553bc3bd7e0c93
partsguy73 <lellodrian@gmail.com>
2024-10-18 21:36:34 -0600

    week 3 final commit

--- scripts/place.js
+++ scripts/place.js
@@ -1,13 +1,24 @@
 function calculateWindChill() {
-    const temperature = document.getElementById('temperature').value;
-    const windSpeed = document.getElementById('windSpeed').value;
+    const temperature = document.getElementById('temperature').innerText;
+    const windSpeed = document.getElementById('windSpeed').innerText;
 
     if (temperature === "" || windSpeed === "") {
         alert("Please enter both temperature and wind speed values.");
         return;
     }
 
-    const windChill = 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+    const temp = parseFloat(temperature);
+    const speed = parseFloat(windSpeed);
 
-    document.getElementById('windChillResult').textContent = windChill.toFixed(2) + " °F";
+    var windChillValue = "N/A"
+    console.log(temperature);
+    console.log(speed);
+    if ((temp <= 10 && speed > 4.8)) {
+        const windChill = 13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * temp * Math.pow(speed, 0.16));
+        windChillValue = windChill.toFixed(2) + " °C"
+    }
+
+    document.getElementById('windChillResult').textContent = windChillValue;
 }
+
+calculateWindChill();
