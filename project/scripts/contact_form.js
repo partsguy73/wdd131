daae4aa3c51e16a358b43f630a265428a15bc527
partsguy73 <lellodrian@gmail.com>
2024-10-30 20:22:43 -0600

    contact js change

--- project/scripts/contact_form.js
+++ project/scripts/contact_form.js
@@ -1,30 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('.reviewForm');
-    const submitButton = document.getElementById('submitButton');
+let formsSended = window.localStorage.getItem("forms") || 0
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault(); 
 
-        const firstName = document.getElementById('firstName').value;
-        const lastName = document.getElementById('lastName').value;
-        const email = document.getElementById('email').value;
-        const chorus = document.getElementById('chorus').checked;
-        const quartet = document.getElementById('quartet').checked;
-        const sing = document.getElementById('sing').checked;
-        const support = document.getElementById('support').checked;
-        const review = document.getElementById('review').value;
-
-        const formData = {
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            chorus: chorus,
-            quartet: quartet,
-            sing: sing,
-            support: support,
-            review: review
-        };
-
-        localStorage.setItem('formData', JSON.stringify(formData));
-    });
+const submit = document.getElementById('submitButton')
+submit.addEventListener('click', (e) => {
+    formsSended++;
+    window.localStorage.setItem("forms", formsSended);
 });
